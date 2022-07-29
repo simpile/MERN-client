550a89576de930e2965162378f66ce4b1778d5fc
simpile <nasim.pilevar@gmail.com>
2022-07-29 22:56:22 +0430

    manage input Validation with useReducer

--- src/posts/pages/NewPost.js
+++ src/posts/pages/NewPost.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Button from '../../shares/FormElemants/Button'
 import Input from '../../shares/FormElemants/Input'
 
 function NewPost() {
   return (
 <div>
   <form>
-    <Input element="input" placeholder="title" type="text"/>
-    <Input />
+    <Input element="input" placeholder="title" type="text" errorText="unvalid text title"/>
+    <Input placeholder="discription" errorText="unvalid text title"/>
+    <Button>submit</Button>
   </form>
 </div>
     )
--- src/shares/FormElemants/Input.js
+++ src/shares/FormElemants/Input.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
+const inputReducer=(state, action)=>{
+  switch(action.type){
+    case"CHANGE"
+    :return{...state,
+      value:action.val,
+      isValid:true
+    }
+  }
+
+}
 function Input(props) {
+  const[inputState, dispatch]= useReducer(inputReducer,{value:"", isValid:false})
+  const changeHandler =(event)=>{
+dispatch({
+  type:"CHANGE",
+  val:event.target.value
+})
+  }
     const element = props.element === "input"?(<input
     id={props.id}
     type={props.type}
     placeholder={props.placeholder}
+    onChange={changeHandler}
+    value={inputState.value}
     />):(
         <textarea
         id={props.id}
         rows={props.row || 3}
         placeholder={props.placeholder}
+        onChange={changeHandler}
+        value={inputState.value}
         />
     )
   return (
     <div>
         {element}
+        {!inputState.isValid && <p>{props.errorText}</p>}
     </div>
   )
 }
