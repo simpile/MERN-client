254df4568182759c2d50c6d9d4971145237de3d8
simpile <nasim.pilevar@gmail.com>
2022-07-31 23:46:47 +0430

    tying to add useEffect

--- src/posts/pages/NewPost.js
+++ src/posts/pages/NewPost.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Button from '../../shares/FormElemants/Button'
 import Input from '../../shares/FormElemants/Input'
 import {validatorRequire} from'../../shares/utils/validators'
 
 function NewPost() {
+  const inputHandler =useCallback((id, value, isValid)=>{},[])
   return (
 <div>
   <form>
-    <Input  element="input" placeholder="title" type="text" errorText="unvalid text title" validators={validatorRequire()}/>
+    <Input onInput={inputHandler}  element="input" placeholder="title" type="text" errorText="unvalid text title" validators={validatorRequire()}/>
     <Input placeholder="discription" errorText="unvalid text title" validators={validatorRequire()}/>
     <Button>submit</Button>
   </form>
--- src/shares/FormElemants/Input.js
+++ src/shares/FormElemants/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import {validate} from '../utils/validators'
 
 const inputReducer=(state, action)=>{
@@ -8,25 +8,49 @@ const inputReducer=(state, action)=>{
       value:action.val,
       isValid:validate(action.val, action.validators),
     }
+    case "BLUR":
+      return{
+      ...state,
+      isBlured:true
+    }
     default: return state
   }
 
 }
+
 function Input(props) {
-  const[inputState, dispatch]= useReducer(inputReducer,{value:"", isValid:false})
-  const changeHandler =(event)=>{
+  
+const[inputState, dispatch]= useReducer(inputReducer,{value:"", isValid:false, isBlured:false})
+const {id, onInput}= props;
+const {value, isValid}= inputState;
+useEffect(()=>{
+  onInput(id, value, isValid)
+},[id, value, isValid]) 
+
+
+const changeHandler =(event)=>{
 dispatch({
   type:"CHANGE",
   val:event.target.value,
   validators:props.validators
 })
   }
+
+const onBlurHandler =(e)=>{
+  dispatch({
+    type:'BLUR',
+
+  })
+}
+
     const element = props.element === "input"?(<input
     id={props.id}
     type={props.type}
     placeholder={props.placeholder}
     onChange={changeHandler}
     value={inputState.value}
+    onBlur={onBlurHandler}
+
     />):(
         <textarea
         id={props.id}
@@ -34,12 +58,15 @@ dispatch({
         placeholder={props.placeholder}
         onChange={changeHandler}
         value={inputState.value}
+        onBlur={onBlurHandler}
         />
     )
   return (
     <div>
         {element}
-        {!inputState.isValid && <p>{props.errorText}</p>}
+        {!inputState.isValid &&
+        inputState.isBlured &&
+        <p>{props.errorText}</p>}
     </div>
   )
 }
