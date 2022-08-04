c622c0cb8cbcaff42000c1f79d69bbee0d43ae31
simpile <nasim.pilevar@gmail.com>
2022-08-04 23:31:21 +0430

    costume hooks

--- src/posts/pages/NewPost.js
+++ src/posts/pages/NewPost.js
@@ -1,39 +1,13 @@
-import React, { useCallback, useReducer } from 'react'
+import React from 'react'
 import Button from '../../shares/FormElemants/Button'
 import Input from '../../shares/FormElemants/Input'
+import { useForm } from '../../shares/hooks/useForm'
 import {validatorRequire} from'../../shares/utils/validators'
 
-const formReducer=(statee, action)=>{
-  switch(action.type){
-    case'INPUT-CHANGE':
-      let formIsValid = true
-      for (const inputId in statee.inputs){
-        if(inputId===action.inputId){
-        formIsValid = formIsValid && action.isValid}
-         else{
-          formIsValid = formIsValid && statee.inputs[inputId].isValid
-        }
-      }
-      return{
-        ...statee,
-        inputs:{
-        ...statee.inputs,
-        [action.inputId]:{
-          value:action.value,
-          isValid:action.isValid}
-        },
-        isValid:formIsValid
-        
-      }
-      default: 
-      return statee
-  }
 
-}
 
 function NewPost() {
-  const [formState, formDispatch]=useReducer(formReducer,{
-inputs:{
+const [formState, inputHandler]=useForm({
   postTitle:{
 valu:"",
 isValid:false
@@ -42,15 +16,8 @@ isValid:false
     valu:"",
     isValid:false
   }
-} ,
-  isValid:false})
-    const inputHandler =useCallback((id, value, isValid)=>{
-formDispatch({
-  type:'INPUT-CHANGE',
-  value:value,
-  isValid:isValid,
-  inputId:id
-})},[])
+}, false )
+
 const onSubmitHandler =(event)=>{
 event.preventDefault()
 console.log(formState.inputs)
---
+++ src/shares/hooks/useForm.js
@@ -0,0 +1,42 @@
+import React, { useCallback, useReducer } from 'react'
+const formReducer=(statee, action)=>{
+    switch(action.type){
+      case'INPUT-CHANGE':
+        let formIsValid = true
+        for (const inputId in statee.inputs){
+          if(inputId===action.inputId){
+          formIsValid = formIsValid && action.isValid}
+           else{
+            formIsValid = formIsValid && statee.inputs[inputId].isValid
+          }
+        }
+        return{
+          ...statee,
+          inputs:{
+          ...statee.inputs,
+          [action.inputId]:{
+            value:action.value,
+            isValid:action.isValid}
+          },
+          isValid:formIsValid
+          
+        }
+        default: 
+        return statee
+    }
+  
+  }
+export const useForm=(intialState, intialValidation)=>{
+    const [formState, formDispatch]=useReducer(formReducer,{
+        inputs:intialState,
+          isValid:intialValidation})
+          const inputHandler =useCallback((id, value, isValid)=>{
+            formDispatch({
+              type:'INPUT-CHANGE',
+              value:value,
+              isValid:isValid,
+              inputId:id
+            })},[])
+
+            return[formState, inputHandler]
+}
